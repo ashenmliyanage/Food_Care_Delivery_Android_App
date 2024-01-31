5323a25bdd86976157ab9076d5a104df6bb9d665
AshenMadhushanka <glamadushanka2@gmail.com>
2024-01-31 17:29:08 +0530

    Login Page Design Completed

---
+++ app/src/main/java/com/example/fooddelivery/Activity/introActivity.java
@@ -0,0 +1,14 @@
+package com.example.fooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class introActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
